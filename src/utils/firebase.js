88b6dca80b2987316c8f802e5e14a4edcf9ddce4
M. Abhysta Huda Arrofif <76678840+Abhysta@users.noreply.github.com>
2024-06-29 12:11:25 +0700

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8D5xvGhf3LJ_5A11WE7deqzqxluAsBmo",
-  authDomain: "job-data-c1395.firebaseapp.com",
-  projectId: "job-data-c1395",
-  storageBucket: "job-data-c1395.appspot.com",
-  messagingSenderId: "730540447099",
-  appId: "1:730540447099:web:3712d0a415495b612859de",
-  measurementId: "G-8C6XHW461K",
+  apiKey: "secret",
+  authDomain: "secret",
+  projectId: "secret",
+  storageBucket: "secret",
+  messagingSenderId: "secret",
+  appId: "secret",
+  measurementId: "secret",
 };
 
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app, "gs://job-data-c1395.appspot.com");
+const storage = getStorage(app, "secret");
 export { storage };
